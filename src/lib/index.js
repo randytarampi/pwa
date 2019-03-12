67fbab9b7c05e934b2f259173378c7bc0e1f176a
Randy Tarampi <rt@randytarampi.ca>
2019-03-11 17:21:39 -0700

    chore: Clarify `output` -> `outputDirectory`.

--- src/lib/index.js
+++ src/lib/index.js
@@ -5,33 +5,33 @@ const colors = require("colors");
 
 const errorMessage = error => console.error(colors.red("ERROR"), typeof error === "string" ? error : error.message); // eslint-disable-line no-console
 
-const generateAssetDestination = (output, template) => path.join(output, template.name);
+const generateAssetDestination = (outputDirectory, template) => path.join(outputDirectory, template.name);
 
-const generateIcon = (image, output, iconTemplate) => image
+const generateIcon = (image, outputDirectory, iconTemplate) => image
     .clone()
     .resize(iconTemplate.width, iconTemplate.height || iconTemplate.width)
-    .toFile(generateAssetDestination(output, iconTemplate))
+    .toFile(generateAssetDestination(outputDirectory, iconTemplate))
     .then(() => {
         console.info(colors.green("OK"), "Image resized to", iconTemplate.width, "x", // eslint-disable-line no-console
-            iconTemplate.height || iconTemplate.width, "and written to", output);
+            iconTemplate.height || iconTemplate.width, "and written to", outputDirectory);
     })
     .catch(errorMessage);
 
-const generateSplashScreen = (image, output, splashScreenTemplate) => image
+const generateSplashScreen = (image, outputDirectory, splashScreenTemplate) => image
     .clone()
     .resize(splashScreenTemplate.width, splashScreenTemplate.height || splashScreenTemplate.width)
-    .toFile(generateAssetDestination(output, splashScreenTemplate))
+    .toFile(generateAssetDestination(outputDirectory, splashScreenTemplate))
     .then(() => {
         console.info(colors.green("OK"), "Image scaled and cropped to", splashScreenTemplate.width, "x", // eslint-disable-line no-console
-            splashScreenTemplate.height || splashScreenTemplate.width, "and written to", output);
+            splashScreenTemplate.height || splashScreenTemplate.width, "and written to", outputDirectory);
     })
     .catch(errorMessage);
 
-const generateAssetsGenerator = generator => (templates, input, output) => {
+const generateAssetsGenerator = generator => (templates, input, outputDirectory) => {
     const image = sharp(input);
 
     return new Promise((resolve, reject) => {
-        mkdirp(path.resolve(output), error => {
+        mkdirp(path.resolve(outputDirectory), error => {
             if (error) {
                 return reject(error);
             }
@@ -39,7 +39,7 @@ const generateAssetsGenerator = generator => (templates, input, output) => {
             resolve();
         });
     })
-        .then(() => Promise.all(templates.map(template => generator(image, output, template))));
+        .then(() => Promise.all(templates.map(template => generator(image, outputDirectory, template))));
 };
 
 module.exports = {
